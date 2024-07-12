2724bfb2d0ea570eb36bf1c831a890d8f2f23690
efa07 <efatariku07@gmail.com>
2024-07-12 04:29:46 +0300

    first commit

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+-- SQL script that ranks country origins of bands
+-- Ordered by the number of (non-unique) fans
+-- Script can be executed on any database
+
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
